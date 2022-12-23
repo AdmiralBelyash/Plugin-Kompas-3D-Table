ba2b3c8cc9758f1d5956227de1e8656d6daf5bea
AdmiralBelyash <71133779+AdmiralBelyash@users.noreply.github.com>
2022-12-23 22:04:26 +0700

    Add missing tests

--- src/Tests/TestBuilder/TestBuilder.cs
+++ src/Tests/TestBuilder/TestBuilder.cs
@@ -27,20 +27,36 @@ public class TestTableBuilder
     /// </summary>
     private TableBuilder TableBuilder => new TableBuilder();
 
-    [TestCase(TestName = "Тестирование построения корректного стола.")]
+    [TestCase(TestName = "Тестирование построения корректного стола без закруглений.")]
     public void TestBuildTable_DoesNotThrowException()
     {
         var tableBuilder = TableBuilder;
         var testApiService = TestApiService;
         var tableParameters = TableParameters;
 
-        Assert.DoesNotThrow(() => tableBuilder.BuildTable(tableParameters, testApiService), "Произошла ошибка при построении.");
+        Assert.DoesNotThrow(() => tableBuilder.BuildTable(tableParameters, testApiService), "Не произошла ошибка при построении.");
         Assert.IsTrue(testApiService.IsCreateDocument, "Документ создан.");
         Assert.IsTrue(testApiService.IsCreateNewSketch, "Эскиз создан.");
         Assert.IsTrue(testApiService.IsCreatePoint, "Точка создана.");
         Assert.IsTrue(testApiService.IsExtrude, "Выдавливание выполнено.");
-        Assert.IsTrue(testApiService.IsRounded, "Скругление углов выполнено.");
         Assert.IsTrue(testApiService.IsCreateRectangle, "Прямоугольник создан.");
+        Assert.IsFalse(testApiService.IsRounded, "Скругление углов выполнено.");
+    }
 
+    [TestCase(TestName = "Тестирование построения корректного стола со закруглений.")]
+    public void TestBuildTable_DoesNotThrowExceptionWithRound()
+    {
+        var tableBuilder = TableBuilder;
+        var testApiService = TestApiService;
+        var tableParameters = TableParameters;
+        tableParameters.TableParameterCollection[ParameterType.TableCornerRadius].Value = 0.5;
+
+        Assert.DoesNotThrow(() => tableBuilder.BuildTable(tableParameters, testApiService), "Не произошла ошибка при построении.");
+        Assert.IsTrue(testApiService.IsCreateDocument, "Документ создан.");
+        Assert.IsTrue(testApiService.IsCreateNewSketch, "Эскиз создан.");
+        Assert.IsTrue(testApiService.IsCreatePoint, "Точка создана.");
+        Assert.IsTrue(testApiService.IsExtrude, "Выдавливание выполнено.");
+        Assert.IsTrue(testApiService.IsCreateRectangle, "Прямоугольник создан.");
+        Assert.IsTrue(testApiService.IsRounded, "Скругление углов выполнено.");
     }
 }
--- src/Tests/TestModel/TestParameter.cs
+++ src/Tests/TestModel/TestParameter.cs
@@ -13,10 +13,32 @@ namespace TestCore;
 [TestFixture]
 public class TestParameter
 {
+    [TestCase(TestName = "Проверка ввода параметров")]
+    public void TestParameter_SetCorrectValues()
+    {
+        var parameter = new Parameter();
+        parameter.Name = "Test";
+        parameter.MinValue = 10;
+        parameter.MaxValue = 12;
+        Assert.That(parameter.MinValue,
+            Is.EqualTo(10),
+            "Негативное значение превратилось в ноль.");
+        Assert.That(parameter.MaxValue,
+            Is.EqualTo(12),
+            "Негативное значение превратилось в ноль.");
+        Assert.That(parameter.Name,
+            Is.EqualTo("Test"),
+            "Присвоилось корректное имя");
+        Assert.That(parameter.ErrorMessage,
+            Is.EqualTo(null),
+            "Ошибок нет");
+    }
+
     [TestCase(TestName = "Проверка ввода негативного значения")]
     public void TestNegativeValueToZero_InCorrectValue()
     {
         var parameter = new Parameter();
+        parameter.Name = "Test";
         parameter.MinValue = -10;
         parameter.MaxValue = -10;
         Assert.That(parameter.MinValue,
@@ -25,6 +47,5 @@ public class TestParameter
         Assert.That(parameter.MaxValue,
             Is.EqualTo(0),
             "Негативное значение превратилось в ноль.");
-
     }
 }
